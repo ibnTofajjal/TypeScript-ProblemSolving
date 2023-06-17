d9dbebb2da5bdb31cd72181e6818e90fd99bb843
ibntofajjal <joynal.bintofajjal@gmail.com>
2023-06-17 12:51:10 +0600

    3

--- src/03-optional-properties.problem.ts
+++ src/03-optional-properties.problem.ts
@@ -1,6 +1,11 @@
 import { expect, it } from "vitest";
 
-export const getName = (params: { first: string; last: string }) => {
+interface INameArgs {
+  first: string;
+  last?: string;
+}
+
+export const getName = (params: INameArgs) => {
   if (params.last) {
     return `${params.first} ${params.last}`;
   }
